aaeae17db28ed62c93339d9a0501e011cbc16e5b
ryugayeon <fbrkdus002@gmail.com>
2025-02-06 20:15:08 +0900

    인풋 추가, 플레이어 이동 추가

--- Source/Project_Pluto/Private/PlayerZagreus.cpp
+++ Source/Project_Pluto/Private/PlayerZagreus.cpp
@@ -4,12 +4,15 @@
 #include "PlayerZagreus.h"
 #include "GameFramework/SpringArmComponent.h"
 #include "Camera/CameraComponent.h"
+#include "../../../../Plugins/EnhancedInput/Source/EnhancedInput/Public/EnhancedInputComponent.h"
 
 // Sets default values
 APlayerZagreus::APlayerZagreus()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	// GetWorld()->GetFirstPlayerController()->bShowMouseCursor = true; // 갑자기 언리얼 오류 남
 
 	// 플레이어 메시 에셋 및 초기 위치 각도 설정
 	ConstructorHelpers::FObjectFinder<USkeletalMesh> TempMesh(TEXT("/Script/Engine.SkeletalMesh'/Game/RGY/Modelings/ParagonGreystone/Characters/Heroes/Greystone/Meshes/Greystone.Greystone'"));
@@ -45,6 +48,12 @@ void APlayerZagreus::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	{
+		// 플레이어 이동
+		PlayerDir.Normalize();
+		SetActorLocation(GetActorLocation() + PlayerDir * Speed * DeltaTime);
+		PlayerDir = FVector::ZeroVector;
+	}
 }
 
 // Called to bind functionality to input
@@ -52,5 +61,60 @@ void APlayerZagreus::SetupPlayerInputComponent(UInputComponent* PlayerInputCompo
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	auto PlayerInput = Cast<UEnhancedInputComponent>(PlayerInputComponent);
+	if (PlayerInput) {
+		// 아예 키 입력이 안 먹히는데 원인 조사 필요
+		PlayerInput->BindAction(IA_Move, ETriggerEvent::Triggered, this, &APlayerZagreus::Move);
+		PlayerInput->BindAction(IA_Attack, ETriggerEvent::Started, this, &APlayerZagreus::Attack);
+		PlayerInput->BindAction(IA_Dodge, ETriggerEvent::Started, this, &APlayerZagreus::Dodge);
+		PlayerInput->BindAction(IA_SpecialAtt, ETriggerEvent::Started, this, &APlayerZagreus::SpecialAtt);
+		PlayerInput->BindAction(IA_Spell, ETriggerEvent::Started, this, &APlayerZagreus::Spell);
+		PlayerInput->BindAction(IA_Interaction, ETriggerEvent::Started, this, &APlayerZagreus::Interaction);
+	}
 }
 
+void APlayerZagreus::SetAttackDir()
+{
+	auto PlayerController = GetWorld()->GetFirstPlayerController();
+	if (PlayerController) {
+		float MouseX, MouseY;
+		PlayerController->GetMousePosition(MouseX, MouseY);
+
+		MouseLocation.X = MouseX;
+		MouseLocation.Y = MouseY;
+
+		AttackDirection = MouseLocation - GetActorLocation();
+	}
+}
+
+void APlayerZagreus::Move(const FInputActionValue& inputValue)
+{
+	FVector2D value = inputValue.Get<FVector2D>();
+	PlayerDir.X = value.X;
+	PlayerDir.Y = value.Y;
+}
+
+void APlayerZagreus::Attack(const FInputActionValue& inputValue)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Attack!"));
+	SetAttackDir();
+	UE_LOG(LogTemp, Warning, TEXT("%.1f , %.1f"), MouseLocation.X, MouseLocation.Y);
+}
+
+void APlayerZagreus::Dodge(const FInputActionValue& inputValue)
+{
+}
+
+void APlayerZagreus::SpecialAtt(const FInputActionValue& inputValue)
+{
+	SetAttackDir();
+}
+
+void APlayerZagreus::Spell(const FInputActionValue& inputValue)
+{
+	SetAttackDir();
+}
+
+void APlayerZagreus::Interaction(const FInputActionValue& inputValue)
+{
+}
--- Source/Project_Pluto/Public/PlayerZagreus.h
+++ Source/Project_Pluto/Public/PlayerZagreus.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "../../../../Plugins/EnhancedInput/Source/EnhancedInput/Public/InputActionValue.h"
 #include "PlayerZagreus.generated.h"
 
 UCLASS()
@@ -35,4 +36,48 @@ public:
 	class UCameraComponent* camComp;
 
 
+
+	FVector AttackDirection;
+
+	FVector MouseLocation;
+
+	// 공격할 때 마우스 위치 및 방향
+	void SetAttackDir();
+
+
+	UPROPERTY(EditAnywhere, Category="PlayerSetting")
+	float Speed = 700.0f;
+
+	FVector PlayerDir;
+
+
+public:
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	class UInputMappingContext* IMC_Player;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	class UInputAction* IA_Move;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	class UInputAction* IA_Attack;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	class UInputAction* IA_Dodge;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	class UInputAction* IA_SpecialAtt;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	class UInputAction* IA_Spell;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	class UInputAction* IA_Interaction;
+
+
+	void Move(const FInputActionValue& inputValue); // 이동
+	void Attack(const FInputActionValue& inputValue); // 일반 공격 & 콤보
+	void Dodge(const FInputActionValue& inputValue); // 회피 & 대시
+	void SpecialAtt(const FInputActionValue& inputValue); // 특수 공격 (Q 스킬)
+	void Spell(const FInputActionValue& inputValue); // 마법 (우클릭)
+	void Interaction(const FInputActionValue& inputValue); // 상호작용 (E 버튼)
 };
