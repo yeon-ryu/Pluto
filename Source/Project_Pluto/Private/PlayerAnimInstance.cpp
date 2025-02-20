bf85fd3af56fcfdda0c22a644dd941ad40facb97
ryugayeon <fbrkdus002@gmail.com>
2025-02-20 16:09:39 +0900

    Q 스킬 공격 방향 세팅 타이밍 변경, 검 이펙트 추가

--- Source/Project_Pluto/Private/PlayerAnimInstance.cpp
+++ Source/Project_Pluto/Private/PlayerAnimInstance.cpp
@@ -35,12 +35,12 @@ void UPlayerAnimInstance::AnimNotify_SpecialAttStart()
 	// 이 순간 부터 우선 순위 최상 -> 이동, 공격 등 이 애니메이션이 끝나기 전까지 아무것도 적용할 수 없다.
 	player->bSpecialAtt = true;
 	//player->bForceSpecialAtt = true;
+	player->SetAttackDir();
 }
 
 void UPlayerAnimInstance::AnimNotify_AttackEffect()
 {
 	// 플레이어가 가지고 있는 무기의 공격 범위 ovelap 체크 시작
-	player->SetAttackDir();
 	player->weapon->StartSpecialAtt();
 }
 
